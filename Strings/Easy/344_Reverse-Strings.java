34f044fdade8fa3fbc2a81e4198e63eee5160d77
AKHILESH-create <akhilesh.k19857@gmail.com>
2026-06-11 22:59:28 +0530

    code added

---
+++ Strings/Easy/344_Reverse-Strings.java
@@ -0,0 +1,32 @@
+package Strings.Easy;
+
+class Solution {
+
+    public static void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        char[] s = { 'h', 'e', 'l', 'l', 'o' };
+
+        reverseString(s);
+
+        System.out.print("Output: ");
+        for (char c : s) {
+            System.out.print(c + " ");
+        }
+    }
+}
